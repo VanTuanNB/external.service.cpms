82d5fcd82544d083de59e0aed65f2fb8e3bb9237
v.tuancv3 <v.tuancv3@cloudhms.net>
2024-09-16 23:21:59 +0700

    [FEAT] update feature faculty

--- src/controllers/faculty.controller.ts
+++ src/controllers/faculty.controller.ts
@@ -1,22 +1,22 @@
-import { Required } from '@/core/decorators/validate.decorator';
+import { EModePayload, Required } from '@/core/decorators/validate.decorator';
 import { FacultyService } from '@/services/faculty.service';
 import type { Request, Response } from 'express';
-import { CreateFacultyFilterModel } from './filters/faculty.filter';
+import { CreateFacultyFilterModel, GetInfoFacultyFilterModel, UpdateFacultyFilterModel } from './filters/faculty.filter';
 
 export default class FacultyController {
     private facultyService = new FacultyService();
     constructor() {}
 
-    // public async getList(req: Request, res: Response): Promise<Response> {
-    //     const result = await this.facultyService.getList();
-    //     return res.status(result.status).json(result);
-    // }
+    public async getList(req: Request, res: Response): Promise<Response> {
+        const result = await this.facultyService.getList();
+        return res.status(result.status).json(result);
+    }
 
-    // @Required(GetInfoCurriculumFilterModel, EModePayload.PARAMS)
-    // public async getById(req: Request, res: Response): Promise<Response> {
-    //     const result = await this.facultyService.getById(req.params.id);
-    //     return res.status(result.status).json(result);
-    // }
+    @Required(GetInfoFacultyFilterModel, EModePayload.PARAMS)
+    public async getById(req: Request, res: Response): Promise<Response> {
+        const result = await this.facultyService.getById(req.params.id);
+        return res.status(result.status).json(result);
+    }
 
     @Required(CreateFacultyFilterModel)
     public async create(req: Request, res: Response): Promise<Response> {
@@ -24,15 +24,17 @@ export default class FacultyController {
         return res.status(result.status).json(result);
     }
 
-    // @Required(CreateRoleFilterModel)
-    // public async update(req: Request, res: Response): Promise<Response> {
-    //     const result = await this.facultyService.update(req.body);
-    //     return res.status(result.status).json(result);
-    // }
+    @Required(GetInfoFacultyFilterModel, EModePayload.PARAMS)
+    @Required(UpdateFacultyFilterModel)
+    public async update(req: Request, res: Response): Promise<Response> {
+        const payload = Object.assign(req.body, req.params);
+        const result = await this.facultyService.update(payload);
+        return res.status(result.status).json(result);
+    }
 
-    // @Required(GetInfoCurriculumFilterModel, EModePayload.PARAMS)
-    // public async permanentlyDelete(req: Request, res: Response): Promise<Response> {
-    //     const result = await this.facultyService.permanentlyDelete(req.params.id);
-    //     return res.status(result.status).json(result);
-    // }
+    @Required(GetInfoFacultyFilterModel, EModePayload.PARAMS)
+    public async permanentlyDelete(req: Request, res: Response): Promise<Response> {
+        const result = await this.facultyService.permanentlyDelete(req.params.id);
+        return res.status(result.status).json(result);
+    }
 }
--- src/routes/faculty.route.ts
+++ src/routes/faculty.route.ts
@@ -3,13 +3,13 @@ import { Router } from 'express';
 
 const router: Router = Router();
 const facultyController = new FacultyController();
-// router
-//     .route('/:id')
-//     .get(facultyController.getById.bind(facultyController))
-//     .delete(facultyController.permanentlyDelete.bind(facultyController));
+router
+    .route('/:id')
+    .get(facultyController.getById.bind(facultyController))
+    .delete(facultyController.permanentlyDelete.bind(facultyController));
 router
     .route('/')
-    // .get(facultyController.getList.bind(facultyController))
+    .get(facultyController.getList.bind(facultyController))
     .post(facultyController.create.bind(facultyController));
 
 export default router;
